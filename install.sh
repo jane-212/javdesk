7429d1fa17689ffe396c6f70e7ef00d9e46d44d3
jane <jjt1525@outlook.com>
2024-03-22 23:54:51 +0800

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "cargo bundle --release"
+cargo bundle --release
+
+APP_NAME="Javdesk"
+APPLICATIONS_PATH="/Applications"
+APP_PATH="${APPLICATIONS_PATH}/${APP_NAME}.app"
+
+if [[ -e ${APP_PATH} ]]
+then
+    echo "file ${APP_PATH} exists"
+    rm ${APP_PATH}
+    echo "file ${APP_PATH} removed"
+fi
+
+echo "move ${APP_NAME} to Applications"
+mv ./target/release/bundle/osx/${APP_NAME}.app ${APPLICATIONS_PATH}
